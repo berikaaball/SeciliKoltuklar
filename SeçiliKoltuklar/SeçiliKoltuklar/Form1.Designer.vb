f4a81d04ea1ed540ff639f9ae72d4825e68dffcd
Berika Bal <berikaaball@gmail.com>
2024-07-06 01:53:05 +0300

    SeciliKoltuklar added

---
+++ SeçiliKoltuklar/SeçiliKoltuklar/Form1.Designer.vb
@@ -0,0 +1,237 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.BosMasaRBtn = New System.Windows.Forms.RadioButton()
+        Me.RezerveDoluRBtn = New System.Windows.Forms.RadioButton()
+        Me.DoluRBtn = New System.Windows.Forms.RadioButton()
+        Me.RezerveRBtn = New System.Windows.Forms.RadioButton()
+        Me.GosterBtn = New System.Windows.Forms.Button()
+        Me.Masa8Btn = New System.Windows.Forms.Button()
+        Me.Masa7Btn = New System.Windows.Forms.Button()
+        Me.Masa6Btn = New System.Windows.Forms.Button()
+        Me.Masa5Btn = New System.Windows.Forms.Button()
+        Me.Masa4Btn = New System.Windows.Forms.Button()
+        Me.Masa3Btn = New System.Windows.Forms.Button()
+        Me.Masa2Btn = New System.Windows.Forms.Button()
+        Me.Masa1Btn = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.BosMasaRBtn)
+        Me.GroupBox1.Controls.Add(Me.RezerveDoluRBtn)
+        Me.GroupBox1.Controls.Add(Me.DoluRBtn)
+        Me.GroupBox1.Controls.Add(Me.RezerveRBtn)
+        Me.GroupBox1.Location = New System.Drawing.Point(500, 127)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Size = New System.Drawing.Size(244, 154)
+        Me.GroupBox1.TabIndex = 20
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Göster"
+        '
+        'BosMasaRBtn
+        '
+        Me.BosMasaRBtn.AutoSize = True
+        Me.BosMasaRBtn.Location = New System.Drawing.Point(32, 112)
+        Me.BosMasaRBtn.Name = "BosMasaRBtn"
+        Me.BosMasaRBtn.Size = New System.Drawing.Size(104, 20)
+        Me.BosMasaRBtn.TabIndex = 3
+        Me.BosMasaRBtn.TabStop = True
+        Me.BosMasaRBtn.Text = "Boş Masalar"
+        Me.BosMasaRBtn.UseVisualStyleBackColor = True
+        '
+        'RezerveDoluRBtn
+        '
+        Me.RezerveDoluRBtn.AutoSize = True
+        Me.RezerveDoluRBtn.Location = New System.Drawing.Point(32, 85)
+        Me.RezerveDoluRBtn.Margin = New System.Windows.Forms.Padding(4)
+        Me.RezerveDoluRBtn.Name = "RezerveDoluRBtn"
+        Me.RezerveDoluRBtn.Size = New System.Drawing.Size(174, 20)
+        Me.RezerveDoluRBtn.TabIndex = 2
+        Me.RezerveDoluRBtn.Text = "Rezerve ve Dolu Olanlar"
+        Me.RezerveDoluRBtn.UseVisualStyleBackColor = True
+        '
+        'DoluRBtn
+        '
+        Me.DoluRBtn.AutoSize = True
+        Me.DoluRBtn.Location = New System.Drawing.Point(32, 57)
+        Me.DoluRBtn.Margin = New System.Windows.Forms.Padding(4)
+        Me.DoluRBtn.Name = "DoluRBtn"
+        Me.DoluRBtn.Size = New System.Drawing.Size(102, 20)
+        Me.DoluRBtn.TabIndex = 1
+        Me.DoluRBtn.Text = "Dolu Olanlar"
+        Me.DoluRBtn.UseVisualStyleBackColor = True
+        '
+        'RezerveRBtn
+        '
+        Me.RezerveRBtn.AutoSize = True
+        Me.RezerveRBtn.Checked = True
+        Me.RezerveRBtn.Location = New System.Drawing.Point(32, 28)
+        Me.RezerveRBtn.Margin = New System.Windows.Forms.Padding(4)
+        Me.RezerveRBtn.Name = "RezerveRBtn"
+        Me.RezerveRBtn.Size = New System.Drawing.Size(125, 20)
+        Me.RezerveRBtn.TabIndex = 0
+        Me.RezerveRBtn.TabStop = True
+        Me.RezerveRBtn.Text = "Rezerve Olanlar"
+        Me.RezerveRBtn.UseVisualStyleBackColor = True
+        '
+        'GosterBtn
+        '
+        Me.GosterBtn.Location = New System.Drawing.Point(500, 292)
+        Me.GosterBtn.Margin = New System.Windows.Forms.Padding(4)
+        Me.GosterBtn.Name = "GosterBtn"
+        Me.GosterBtn.Size = New System.Drawing.Size(244, 50)
+        Me.GosterBtn.TabIndex = 19
+        Me.GosterBtn.Text = "Göster"
+        Me.GosterBtn.UseVisualStyleBackColor = True
+        '
+        'Masa8Btn
+        '
+        Me.Masa8Btn.Location = New System.Drawing.Point(329, 278)
+        Me.Masa8Btn.Margin = New System.Windows.Forms.Padding(4)
+        Me.Masa8Btn.Name = "Masa8Btn"
+        Me.Masa8Btn.Size = New System.Drawing.Size(121, 53)
+        Me.Masa8Btn.TabIndex = 18
+        Me.Masa8Btn.Tag = "Rezerve"
+        Me.Masa8Btn.Text = "Masa 8"
+        Me.Masa8Btn.UseVisualStyleBackColor = True
+        '
+        'Masa7Btn
+        '
+        Me.Masa7Btn.Location = New System.Drawing.Point(329, 201)
+        Me.Masa7Btn.Margin = New System.Windows.Forms.Padding(4)
+        Me.Masa7Btn.Name = "Masa7Btn"
+        Me.Masa7Btn.Size = New System.Drawing.Size(121, 53)
+        Me.Masa7Btn.TabIndex = 17
+        Me.Masa7Btn.Tag = "Dolu"
+        Me.Masa7Btn.Text = "Masa 7"
+        Me.Masa7Btn.UseVisualStyleBackColor = True
+        '
+        'Masa6Btn
+        '
+        Me.Masa6Btn.Location = New System.Drawing.Point(329, 127)
+        Me.Masa6Btn.Margin = New System.Windows.Forms.Padding(4)
+        Me.Masa6Btn.Name = "Masa6Btn"
+        Me.Masa6Btn.Size = New System.Drawing.Size(121, 53)
+        Me.Masa6Btn.TabIndex = 16
+        Me.Masa6Btn.Tag = "Dolu"
+        Me.Masa6Btn.Text = "Masa 6"
+        Me.Masa6Btn.UseVisualStyleBackColor = True
+        '
+        'Masa5Btn
+        '
+        Me.Masa5Btn.Location = New System.Drawing.Point(196, 278)
+        Me.Masa5Btn.Margin = New System.Windows.Forms.Padding(4)
+        Me.Masa5Btn.Name = "Masa5Btn"
+        Me.Masa5Btn.Size = New System.Drawing.Size(121, 53)
+        Me.Masa5Btn.TabIndex = 15
+        Me.Masa5Btn.Tag = "Boş"
+        Me.Masa5Btn.Text = "Masa 5"
+        Me.Masa5Btn.UseVisualStyleBackColor = True
+        '
+        'Masa4Btn
+        '
+        Me.Masa4Btn.Location = New System.Drawing.Point(196, 127)
+        Me.Masa4Btn.Margin = New System.Windows.Forms.Padding(4)
+        Me.Masa4Btn.Name = "Masa4Btn"
+        Me.Masa4Btn.Size = New System.Drawing.Size(121, 53)
+        Me.Masa4Btn.TabIndex = 14
+        Me.Masa4Btn.Tag = "Dolu"
+        Me.Masa4Btn.Text = "Masa 4"
+        Me.Masa4Btn.UseVisualStyleBackColor = True
+        '
+        'Masa3Btn
+        '
+        Me.Masa3Btn.Location = New System.Drawing.Point(57, 278)
+        Me.Masa3Btn.Margin = New System.Windows.Forms.Padding(4)
+        Me.Masa3Btn.Name = "Masa3Btn"
+        Me.Masa3Btn.Size = New System.Drawing.Size(121, 53)
+        Me.Masa3Btn.TabIndex = 13
+        Me.Masa3Btn.Tag = "Boş"
+        Me.Masa3Btn.Text = "Masa 3"
+        Me.Masa3Btn.UseVisualStyleBackColor = True
+        '
+        'Masa2Btn
+        '
+        Me.Masa2Btn.Location = New System.Drawing.Point(57, 201)
+        Me.Masa2Btn.Margin = New System.Windows.Forms.Padding(4)
+        Me.Masa2Btn.Name = "Masa2Btn"
+        Me.Masa2Btn.Size = New System.Drawing.Size(121, 53)
+        Me.Masa2Btn.TabIndex = 12
+        Me.Masa2Btn.Tag = "Boş"
+        Me.Masa2Btn.Text = "Masa 2"
+        Me.Masa2Btn.UseVisualStyleBackColor = True
+        '
+        'Masa1Btn
+        '
+        Me.Masa1Btn.Location = New System.Drawing.Point(57, 127)
+        Me.Masa1Btn.Margin = New System.Windows.Forms.Padding(4)
+        Me.Masa1Btn.Name = "Masa1Btn"
+        Me.Masa1Btn.Size = New System.Drawing.Size(121, 53)
+        Me.Masa1Btn.TabIndex = 11
+        Me.Masa1Btn.Tag = "Rezerve"
+        Me.Masa1Btn.Text = "Masa 1"
+        Me.Masa1Btn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GosterBtn)
+        Me.Controls.Add(Me.Masa8Btn)
+        Me.Controls.Add(Me.Masa7Btn)
+        Me.Controls.Add(Me.Masa6Btn)
+        Me.Controls.Add(Me.Masa5Btn)
+        Me.Controls.Add(Me.Masa4Btn)
+        Me.Controls.Add(Me.Masa3Btn)
+        Me.Controls.Add(Me.Masa2Btn)
+        Me.Controls.Add(Me.Masa1Btn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RezerveDoluRBtn As RadioButton
+    Friend WithEvents DoluRBtn As RadioButton
+    Friend WithEvents RezerveRBtn As RadioButton
+    Friend WithEvents GosterBtn As Button
+    Friend WithEvents Masa8Btn As Button
+    Friend WithEvents Masa7Btn As Button
+    Friend WithEvents Masa6Btn As Button
+    Friend WithEvents Masa5Btn As Button
+    Friend WithEvents Masa4Btn As Button
+    Friend WithEvents Masa3Btn As Button
+    Friend WithEvents Masa2Btn As Button
+    Friend WithEvents Masa1Btn As Button
+    Friend WithEvents BosMasaRBtn As RadioButton
+End Class
---
+++ SeçiliKoltuklar/SeçiliKoltuklar/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+    Private Sub GosterBtn_Click(sender As Object, e As EventArgs) Handles GosterBtn.Click
+        For Each btn As Button In Me.Controls.OfType(Of Button)
+            If BosMasaRBtn.Checked Then
+                If btn.Tag = "Boş" Then
+                    btn.BackColor = Color.Green
+                Else
+                    btn.BackColor = Color.White
+                End If
+
+            ElseIf DoluRBtn.Checked Then
+                If btn.Tag = "Dolu" Then
+                    btn.BackColor = Color.Red
+                Else
+                    btn.BackColor = Color.White
+                End If
+
+            ElseIf RezerveRBtn.Checked Then
+                If btn.Tag = "Rezerve" Then
+                    btn.BackColor = Color.Yellow
+                Else
+                    btn.BackColor = Color.White
+                End If
+
+            ElseIf RezerveDoluRBtn.Checked Then
+                If btn.Tag = "Rezerve" Then
+                    btn.BackColor = Color.Pink
+                ElseIf btn.Tag = "Dolu" Then
+                    btn.BackColor = Color.Pink
+                Else
+                    btn.BackColor = Color.White
+                End If
+            Else
+                MessageBox.Show("Lütfen bir masa tipi seçiniz.")
+            End If
+        Next
+    End Sub
+
+End Class
